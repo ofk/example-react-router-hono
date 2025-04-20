1bf8e6e294cfbc21bb969b4f0677fe961e8ae3a3
ofk <ofkjpn+github@gmail.com>
2025-04-20 13:19:56 +0900

    Fix SPA routing

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import { Hono } from 'hono';
 
-const app = new Hono();
+interface AppEnv {
+  Bindings: {
+    ASSETS: { fetch: typeof fetch };
+  };
+}
+
+const app = new Hono<AppEnv>();
 
 app.use(async (c, next) => {
   await next();
@@ -12,6 +18,14 @@ const _route = app.get('/api', (c) =>
     message: 'Hello',
   }),
 );
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (!import.meta.env?.DEV) {
+  app.get('*', async (c) => {
+    const resp = await c.env.ASSETS.fetch(c.req.raw.url.slice(0, -c.req.path.length), c.req.raw);
+    return new Response(resp.body, resp);
+  });
+}
 
 export type AppType = typeof _route;
 
